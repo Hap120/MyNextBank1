96fe65ba819941f2d977c9b38919350554227707
himali <himalipethe@gmail.com>
2017-01-29 20:49:05 -0800

    Practice check in

---
+++ MyNextBank1/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNextBank1
+{
+    class Account
+    {
+        #region Statics
+        private static int lastAccountNumber = 0;
+
+        #endregion
+
+        #region Properties
+        public int AccountNumber { get; private set; }
+
+        public string EmailAddress { get; set; }
+
+        public decimal Balance { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        //constructor
+        public Account() : this("", 0.0M)
+        {
+           // this.AccountNumber = ++lastAccountNumber;
+        }
+
+        public Account(string emailAddress) : this(emailAddress, 0.0M)
+        {
+         //   this.AccountNumber = ++lastAccountNumber;
+          //  this.EmailAddress = emailAddress;
+        }
+
+        public Account(string emailAddress, decimal balance)
+        {
+            this.AccountNumber = ++lastAccountNumber;
+            this.EmailAddress = emailAddress;
+            this.Balance = balance;
+
+            
+
+
+        }
+        public decimal Deposit(decimal amount)
+        {
+            // Balance= Balance + amount;
+            Balance += amount;
+            return Balance;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ MyNextBank1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNextBank1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Welcome to my Bank**");
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Create an account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("Please select one of the options above");
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        Console.WriteLine("Please enter your email address");
+                        var emailAddress = Console.ReadLine();
+                        var myAccount = new Account(emailAddress);
+                        Console.WriteLine($"The balance in my account - {myAccount.AccountNumber} is {myAccount.Balance:C} ");
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        Console.WriteLine("Sorry option not available");
+                        break;
+                }
+            }
+
+        }
+    }
+}
